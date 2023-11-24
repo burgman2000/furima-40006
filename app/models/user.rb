d086a4fc75e2e40f499b5cfc595ce71a751adcce
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-24 11:50:05 +0900

    ユーザー登録とテストコード作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,14 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname, presence: true
-         validates :first_name, presence: true
-         validates :last_name, presence: true
-         validates :first_name_kana, presence: true
-         validates :last_name_kana, presence: true
+         validates :nickname, presence: true 
+         validates :first_name, presence: true,  format: { with: /\A[^\p{ASCII}]+\z/, message: 'は全角で入力してください' }
+         validates :last_name, presence: true,  format: { with: /\A[^\p{ASCII}]+\z/, message: 'は全角で入力してください' }
+         validates :first_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: 'は全角カタカナで入力してください' }
+         validates :last_name_kana, presence: true, format: { with: /\A[\p{Katakana}\p{Blank}ー－]+\z/, message: 'は全角カタカナで入力してください' }
          validates :birth_day, presence: true
+
+         PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+         validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 
 end
