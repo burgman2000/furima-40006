1e604ec25dce763b74b43714bd9ac3ceca78b490
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-25 18:10:48 +0900

    出品画面へアクセス

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,14 @@
 class ProductsController < ApplicationController
+
+  before_action :authenticate_user!, except: [:index, :destroy, :edit, :update, :show]
+
+
+
+
   def index
   end
+
+  def new
+  end
+  
 end
