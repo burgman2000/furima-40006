f651bd5777e87c9c987331cf1ab2ed15c2a2a840
burgman2000 <148961588+burgman2000@users.noreply.github.com>
2023-11-28 09:10:31 +0900

    プルダウンのテストコード作成

--- spec/factories/products.rb
+++ spec/factories/products.rb
@@ -1,5 +1,10 @@
+
 FactoryBot.define do
   factory :product do
-    
+    category_id { Faker::Number.between(from: 2, to: 11) }
+    condition_id { Faker::Number.between(from: 2, to: 7) }
+    shipping_fee_id { Faker::Number.between(from: 2, to: 3) }
+    prefecture_id { Faker::Number.between(from: 2, to: 48) }
+    shipping_day_id { Faker::Number.between(from: 2, to: 4) }
   end
 end
--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Product, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context '商品の出品ができない場合' do
+    before do
+      @product = FactoryBot.build(:product)
+    end
+
+    it 'category_idが1だと登録できない' do
+      @product.category_id = 1
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it 'condition_idが1だと登録できない' do
+      @product.condition_id = 1
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Condition can't be blank")
+    end
+
+    it 'shipping_fee_idが1だと登録できない' do
+      @product.shipping_fee_id = 1
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Shipping fee can't be blank")
+    end
+
+    it 'prefecture_idが1だと登録できない' do
+      @product.prefecture_id = 1
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it 'shipping_day_idが1だと登録できない' do
+      @product.shipping_day_id = 1
+      @product.valid?
+      expect(@product.errors.full_messages).to include("Shipping day can't be blank")
+    end
+
+    # 他のバリデーションに関するテストケースを追加する際、それに対応するコードを記述してください
+  end
 end
